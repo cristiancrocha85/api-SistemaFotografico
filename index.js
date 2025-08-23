3438b940aed4d359416505213a98df4e0fc10ad5
cristiancrocha85 <cristian.crocha@outlook.com>
2025-08-23 10:44:28 -0300

    Correção v1.1

--- index.js
+++ index.js
@@ -8,20 +8,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Importa supabase e rotas
 const supabase = require('./supabase');
 const plataformaRouter = require('./routes/plataforma');
 
-// Usar Rotas
+// Rotas
 app.use('/api/plataforma', plataformaRouter);
 
 // Status da API
 app.get('/api/status', (req, res) => {
-  const ambiente = process.env.SUPABASE_URL.includes('sdqkqzpeoubwesgepveu') ? 'Teste' : 'Produção';
+  const ambiente = process.env.MODO_TESTE === 'true' ? 'Teste' : 'Produção';
   res.json({ status: 'API online', ambiente });
 });
 
 // Porta
 const port = process.env.PORT || 4000;
+const ambiente = process.env.MODO_TESTE === 'true' ? 'Teste' : 'Produção';
+
 app.listen(port, () => {
-  console.log(`API rodando na porta ${port} | Ambiente: ${process.env.SUPABASE_URL.includes('sdqkqzpeoubwesgepveu') ? 'Teste' : 'Produção'}`);
+  console.log(`API rodando na porta ${port} | Ambiente: ${ambiente}`);
 });
