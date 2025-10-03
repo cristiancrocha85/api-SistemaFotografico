a68cab815f6b1b2e83cc921ed9b86080a1edd4b1
cristiancrocha85 <cristian.crocha@outlook.com>
2025-10-02 21:55:00 -0300

    Atualização do Index

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+process.env.TZ = 'America/Sao_Paulo';
+
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
@@ -5,22 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Importa supabase e rotas
-const supabase = require('./supabase');
+// Importar Rotas
 const plataformaRouter = require('./routes/plataforma');
 
-// Rotas
+// Usar Rotas
 app.use('/api/plataforma', plataformaRouter);
 
 // Status da API
 app.get('/api/status', (req, res) => {
-  const ambiente = process.env.MODO_TESTE === 'true' ? 'Teste' : 'Produção';
+  const ambiente = process.env.MODO_TESTE === 'true' ? 'Ambiente de Testes' : 'Ambiente Oficial';
   res.json({ status: 'API online', ambiente });
 });
 
 // Porta
 const port = process.env.PORT || 4000;
-const ambiente = process.env.MODO_TESTE === 'true' ? 'Teste' : 'Produção';
+const ambiente = process.env.MODO_TESTE === 'true' ? 'Ambiente de Testes' : 'Ambiente Oficial';
 
 app.listen(port, () => {
   console.log(`API rodando na porta ${port} | Ambiente: ${ambiente}`);
