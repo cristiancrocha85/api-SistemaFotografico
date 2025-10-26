8d32a4e899ae5aacbda634b31089d7372fe3e55a
cristiancrocha85 <cristian.crocha@outlook.com>
2025-10-25 23:16:56 -0300

    Metodo Inserir route/entrada v.1.1

--- routes/entrada.js
+++ routes/entrada.js
@@ -53,54 +53,6 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Erro inesperado ao buscar a entrada', details: err.message });
   }
 });
-
-/*router.get('/', async (req, res) => {
-  try {
-    const { data, error } = await supabase
-      .from('tb_Entrada')
-      .select(`
-        Id,
-        ent_DataEntrada,
-        ent_TipoEvento,
-        ent_DataEvento,
-        ent_Plataforma,
-        ent_QtdFotosVendidas,
-        ent_ValorTotal,
-        ent_TipoPgto,
-        ent_Status,
-        ent_LiberarSaldo,
-        tb_Agenda (ag_Evento)
-      `)
-      .order('ent_DataEntrada', { ascending: false })
-      .order('Id', { ascending: false });
-
-    if (error) {
-      console.error("Erro ao buscar a entrada:", error.message);
-      return res.status(500).json({ error: 'Erro ao buscar a entrada', details: error.message });
-    }
-
-    // Formata saída
-    const response = (data || []).map(entrada => ({
-      Id: entrada.Id,
-      ent_DataEntrada: entrada.ent_DataEntrada,
-      ent_TipoEvento: entrada.ent_TipoEvento,
-      ent_DataEvento: entrada.ent_DataEvento,
-      ent_Plataforma: entrada.ent_Plataforma,
-      ent_QtdFotosVendidas: entrada.ent_QtdFotosVendidas,
-      ent_ValorTotal: entrada.ent_ValorTotal,
-      ent_TipoPgto: entrada.ent_TipoPgto,
-      ent_Status: entrada.ent_Status,
-      ent_LiberarSaldo: entrada.ent_LiberarSaldo,
-      Evento: entrada.tb_Agenda?.ag_Evento || null,
-      
-    }));
-
-    res.json(response);
-  } catch (err) {
-    console.error("Erro inesperado ao buscar a entrada:", err.message);
-    res.status(500).json({ error: 'Erro inesperado ao buscar a entrada', details: err.message });
-  }
-});*/
 // ===================================
 // POST - Inserir Entrada
 // ===================================
@@ -109,6 +61,17 @@ router.post('/', async (req, res) => {
     const {
       ent_DataEntrada,
       ent_Evento,
+      ent_TipoEvento,
+      ent_DataEvento,
+      ent_Plataforma,
+      ent_QtdFotosVendidas,
+      ent_ValorTotal,
+      ent_TipoPgto,
+      ent_Status,
+      ent_LiberarSaldo,
+      ent_DataPrevista,
+      ent_Mes,
+      ent_Ano
     } = req.body;
 
     // Define data padrão se não vier nada
@@ -120,7 +83,18 @@ router.post('/', async (req, res) => {
       .from('tb_Entrada')
       .insert([{
         ent_DataEntrada: dataStr,
-        ent_Evento: Number(ent_Evento)
+        ent_Evento: Number(ent_Evento),
+        ent_TipoEvento: ent_TipoEvento,
+        ent_DataEvento: ent_DataEvento,
+        ent_Plataforma: ent_Plataforma,
+        ent_QtdFotosVendidas: ent_QtdFotosVendidas,
+        ent_ValorTotal,
+        ent_TipoPgto: ent_TipoPgto,
+        ent_Status: ent_Status,
+        ent_LiberarSaldo:ent_LiberarSaldo,
+        ent_DataPrevista: ent_DataPrevista,
+        ent_Mes: ent_Mes,
+        ent_Ano:ent_Ano
       }])
       .select()
       .single(); // retorna apenas um objeto
