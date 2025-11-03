a31e088a82c0199ebdc23b376d1274a07878b526
cristiancrocha85 <cristian.crocha@outlook.com>
2025-11-03 13:19:10 -0300

    Ajuste no index.js e criação do medias.js

---
+++ routes/medias.js
@@ -0,0 +1,26 @@
+// routes/plataforma.js
+const express = require('express');
+const router = express.Router();
+const supabase = require('../supabase');
+
+//======================================================================
+// 
+//======================================================================
+router.get('/total/:idEvento', async (req, res) => {
+  const { idEvento } = req.params;
+
+  try {
+    const { data, error } = await supabase
+      .from('tb_Entradas')
+      .select('QtdFotos', { count: 'exact' })
+      .eq('IdEvento', idEvento);
+
+    if (error) throw error;
+
+    const totalFotos = data.reduce((sum, row) => sum + (row.QtdFotos || 0), 0);
+    res.status(200).json({ totalFotos });
+  } catch (err) {
+    console.error('Erro ao buscar total de fotos:', err.message);
+    res.status(500).json({ error: 'Erro ao buscar total de fotos', details: err.message });
+  }
+});
--- routes/plataforma.js
+++ routes/plataforma.js
@@ -116,5 +116,4 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-
 module.exports = router;
