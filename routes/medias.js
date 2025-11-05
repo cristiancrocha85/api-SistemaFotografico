0877139d6a046a722c43b4f10eb7aca0bf0dec6f
cristiancrocha85 <cristian.crocha@outlook.com>
2025-11-05 12:34:17 -0300

    Correção routes/medias v1.0

--- routes/medias.js
+++ routes/medias.js
@@ -3,9 +3,6 @@ const express = require('express');
 const router = express.Router();
 const supabase = require('../supabase');
 
-//=========================================================================================
-//Código Medias
-//========================================================================================
 router.get('/totais/:idEvento', async (req, res) => {
   const { idEvento } = req.params;
 
@@ -27,39 +24,5 @@ router.get('/totais/:idEvento', async (req, res) => {
   }
 });
 
-//=========================================================================================
-// Inserir
-//=========================================================================================
-router.post('/', async (req, res) => {
-  try {
-    const {
-      med_FotosVendidas,
-      med_ValorTotal,
-    } = req.body;
-
-    // Inserção mínima no Supabase
-    const { data, error } = await supabase
-      .from('tb_Medias')
-      .insert([{
-        med_FotosVendidas: med_FotosVendidas ? Number(med_FotosVendidas) : 0,
-        med_ValorTotal: med_ValorTotal ? Number(String(med_ValorTotal).replace(',','.')):0,
-      }])
-      .select()
-      .single(); // retorna apenas um objeto
-
-    if (error) {
-      console.error("Erro ao inserir as médias:", error.message);
-      return res.status(500).json({ error: 'Erro ao inserir as médias', details: error });
-    
-      
-    
-    }
-    res.status(201).json(data);
-
-  } catch (err) {
-    console.error("Erro inesperado ao inserir as médias:", err.message);
-    res.status(500).json({ error: 'Erro inesperado ao inserir as médias', details: err.message });
-  }
-});
 
 module.exports = router;
