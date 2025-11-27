4b115d0e1c05fc072a2abf56ccd67f57bbde0da2
cristiancrocha85 <41800146+cristiancrocha85@users.noreply.github.com>
2025-11-26 23:54:43 -0300

    Atualização routes/medias (top 3 meses)

--- routes/medias.js
+++ routes/medias.js
@@ -168,35 +168,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-/*router.post('/', async (req, res) => {
+// =============================================
+// Top 3 Meses
+// =============================================
+router.get("/top3-mes/:mes/:ano", async (req, res) => {
   try {
-    const {
-      med_FotosVendidas,
-      med_ValorTotal,
-    } = req.body;
+    const mes = parseInt(req.params.mes);
+    const ano = parseInt(req.params.ano);
 
-    // Inserção mínima no Supabase
     const { data, error } = await supabase
-      .from('tb_Medias')
-      .insert([{
-        med_FotosVendidas: med_FotosVendidas ? Number(med_FotosVendidas) : 0,
-        med_ValorTotal: med_ValorTotal ? Number(String(med_ValorTotal).replace(',','.')):0,
-      }])
-      .select()
-      .single(); // retorna apenas um objeto
+      .rpc("fn_top3_mes", { m: mes, y: ano });
 
-    if (error) {
-      console.error("Erro ao inserir as médias:", error.message);
-      return res.status(500).json({ error: 'Erro ao inserir as médias', details: error });
-    
-      
-    
-    }
-    res.status(201).json(data);
+    if (error) throw error;
+
+    res.json({ top3: data });
   } catch (err) {
-    console.error("Erro inesperado ao inserir as médias:", err.message);
-    res.status(500).json({ error: 'Erro inesperado ao inserir as médias', details: err.message });
+    res.status(500).json({ error: err.message });
   }
-});*/
+});
 
 module.exports = router;
