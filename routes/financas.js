21448f49294b2292f7d22b34da8e3ddd3d61a48b
cristiancrocha85 <cristian.crocha@outlook.com>
2026-05-13 13:31:04 -0300

    Ajuste na route/finanças saldo bloqueado v2

--- routes/financas.js
+++ routes/financas.js
@@ -404,7 +404,7 @@ router.get('/ajuste_bloqueado', async (req, res) => {
 });
 //==========================================================================
 //===================================================
-// Ajuste Bloqueado
+// Liberar Saldo Bloqueado
 //===================================================
 router.get('/liberar_saldo_bloqueado', async (req, res) => {
   try {
@@ -413,7 +413,7 @@ router.get('/liberar_saldo_bloqueado', async (req, res) => {
 
     if (error) throw error;
 
-    //res.json({ saldoBloqueado: data ?? 0 });
+    res.json({ liberarSaldoBloqueado: data ?? 0 });
   } catch (err) {
     console.error('Erro RPC liberar_saldo_bloqueado:', err);
     res.status(500).json({ erro: 'Falha ao buscar o ajuste _ bloqueado.' });
